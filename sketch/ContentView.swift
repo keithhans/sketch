2235fe90e769ce4020fa207e554454097476ff1a
keithhans <keithhans@gmail.com>
2024-10-23 09:40:18 +0800

    Initial Commit

---
+++ sketch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sketch
+//
+//  Created by 皮卡熊 on 2024/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sketch/sketchApp.swift
@@ -0,0 +1,17 @@
+//
+//  sketchApp.swift
+//  sketch
+//
+//  Created by 皮卡熊 on 2024/10/23.
+//
+
+import SwiftUI
+
+@main
+struct sketchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
